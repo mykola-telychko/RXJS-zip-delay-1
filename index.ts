f9841075cd077859d1b2c59c4adf40d33056d3fb
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-10 21:43:37 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 import { delay } from 'rxjs/operators';
 import { of, zip } from 'rxjs';
 
+// Example 1: zip multiple observables emitting at alternate intervals
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/zip
+
 const srcOne = of('Hello');
 const srcTwo = of('World!');
 const srcThree = of('Goodbye');
 const srcFour = of('World!');
-//wait until all observables have emitted a value then emit all as an array
-const example = zip(
+
+// wait until all observables have emitted a value then emit all as an array
+// zip - wait all values with different delayed
+const exampleSub = zip(
   srcOne,
   srcTwo.pipe(delay(1000)),
   srcThree.pipe(delay(2000)),
   srcFour.pipe(delay(3000))
 );
-//output: ["Hello", "World!", "Goodbye", "World!"]
-const subscribe = example.subscribe((val) => console.log(val));
+const subscribe = exampleSub.subscribe((val) => console.log(val));
